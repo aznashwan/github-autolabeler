15c725b120747723ef879a0ee1c3f5579c39dca0
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2023-07-27 17:54:36 +0300

    Add labelling targets impls and manager.
    Signed-off-by: Nashwan Azhari <nazhari@cloudbasesolutions.com>

--- autolabeler/main.py
+++ autolabeler/main.py
@@ -20,7 +20,7 @@ from io import IOBase
 import github
 import yaml
 
-from autolabeler import labelers
+from autolabeler import manager
 from autolabeler import utils
 
 LOG = utils.getStdoutLogger(__name__)
@@ -54,65 +54,6 @@ def _add_arguments(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     return parser
 
 
-def _parse_target_argument(target: str) -> dict:
-    """ Parses the provided target for the labelling action.
-
-    Supports inputs of the form:
-    - username/repository_name
-    - user/repo/{issues/pulls} for all issues/pulls
-    - user/repo/{issue/pull}/123 for a specific issue/pull
-
-    Returns dict of the form: {
-        "original": "<the original string>",
-        "user": "<user/org owning the repo>",
-        "repo": "<name of the repository>",
-        "type": None/"issue"/"pull",
-        "id": None/int,
-    }
-    """
-    parts = target.split('/')
-    if len(parts) not in range(2, 5):
-        raise ValueError(
-            "Target format must be a slash-separated string with the "
-            "following path elements: username/repository[/type[/id]]. "
-            f"Got: {target} ({len(parts)} path elements)")
-
-    def _safe_index(n: int):
-        try:
-            return parts[n]
-        except IndexError:
-            return None
-
-    target_type = None
-    match _safe_index(2):
-        case None:
-            pass
-        # NOTE: special concession to accept 'pulls/issues' as plurals as well.
-        case 'pull' | 'pulls':
-            target_type = 'pull'
-        case 'issue' | 'issues':
-            target_type = 'issue'
-        case other:
-            accepted = ['pull(s)', 'issue(s)']
-            raise ValueError(
-                f"Unsupported target type '{other}'. Must be one of: {accepted}")
-
-    target_id = None
-    match _safe_index(3):
-        case None:
-            pass
-        case some:
-            target_id = int(some)
-
-    return {
-        "original": target,
-        "user": parts[0],
-        "repo": parts[1],
-        "type": target_type,
-        "id": target_id,
-    }
-
-
 def load_yaml_file(path_or_file: str|IOBase) -> dict:
     file = path_or_file
     if isinstance(path_or_file, str):
@@ -136,33 +77,14 @@ def main():
              "or 'GITHUB_TOKEN' environment variable.")
 
     gh = github.Github(login_or_token=args.github_token)
+    # NOTE(aznashwan): transparent login through __getattr__:
     _ = gh.get_user().login
 
-    lblrs = []
+    rules_config = {}
     if args.label_definitions_file:
         rules_config = load_yaml_file(args.label_definitions_file)
-        lblrs = labelers.load_labelers_from_config(rules_config)
-    LOG.info(f"Loaded following labelers: {lblrs}")
 
-    target = _parse_target_argument(args.target)
-    repo = gh.get_repo(f"{target['user']}/{target['repo']}")
-
-    op = None
-    match target['type']:
-        case None:
-            op = lambda l: l.get_labels_for_repo(repo)
-        case 'pull':
-            pr = repo.get_pull(target['id'])
-            op = lambda l: l.get_labels_for_pr(pr)
-        case 'issue':
-            issue = repo.get_issue(target['id'])
-            op = lambda l: l.get_labels_for_issue(issue)
-        case _:
-            raise ValueError("Unsupported target '%s'")
-
-    labels = []
-    for labeler in lblrs:
-        labels.extend(op(labeler))
-
-    label_dicts = [l.to_dict() for l in labels]
-    print(f"\nResulting labels were: {json.dumps(label_dicts, indent=4)}")
+    label_manager = manager.LabelsManager(gh, args.target, rules_config)
+    labels = label_manager.generate_labels()
+    labels_dicts = [l.to_dict() for l in labels]
+    print(f"\nResulting labels were: {json.dumps(labels_dicts, indent=4)}")
--- autolabeler/targets.py
+++ autolabeler/targets.py
@@ -13,54 +13,170 @@
 #    under the License.
 
 import abc
-import logging
 
 from github import Github
-from github.Label import Label
+from github.Issue import Issue
+from github.PullRequest import PullRequest
 
-LOG = logging.getLogger()
+from autolabeler.labelers import LabelParams
+from autolabeler import utils
+
+
+LOG = utils.getStdoutLogger(__name__)
 
 
 class BaseLabelsTarget(metaclass=abc.ABCMeta):
     """ ABC for GitHub API entities which can have labels applies to them. """
 
-    # @abc.abstractclassmethod
-    # def from_str(cls, client: Github, target_str: str) -> Self:
-    #     LOG.debug(f"BaseLabelsTarget.from_str({client}, {target_str})")
-    #     raise NotImplemented
-
     @abc.abstractmethod
-    def get_labels(self) -> list[Label]:
+    def get_labels(self) -> list[LabelParams]:
         raise NotImplemented
 
     @abc.abstractmethod
-    def set_labels(self, labels: list[Label]):
+    def set_labels(self, labels: list[LabelParams]):
         _ = labels
         raise NotImplemented
 
     @abc.abstractmethod
     def remove_labels(self, labels: list[str]):
         _ = labels
+        raise NotImplemented
+
+    @abc.abstractmethod
+    def get_target_handle(self):
         raise NotImplemented
 
 
 class RepoLabelsTarget(BaseLabelsTarget):
+    """ Abstracts the creation/deletion of labels on Repositories. """
 
     def __init__(self, client: Github, repo_user: str, repo_name: str):
-        self._client = client
-        self._repo_user = repo_user
-        self._repo_name = repo_name
-        self._repo_handle = client.get_repo(f"{repo_user}/{repo_name}")
+        if not all([repo_name, repo_user]):
+            raise ValueError(
+                "Target repository user and name arguments must be provided. "
+                f"Got: {repo_name=} {repo_name=}")
 
-    def get_labels(self) -> list[Label]:
-        return list(self._repo_handle.get_labels())
-    
-    def set_labels(self, labels: list[Label]):
-        for label in labels:
-            self._repo_handle.create_label(label.name, label.color)
+        self._client = client
+        self._user = repo_user
+        self._name = repo_name
+        self._repo = client.get_repo(f"{repo_user}/{repo_name}")
+
+    def get_target_handle(self):
+        return self._repo
+
+    def get_labels(self) -> list[LabelParams]:
+        return [LabelParams.from_label(l) for l in self._repo.get_labels()]
+
+    def set_labels(self, labels: list[LabelParams]):
+        """ Creates or updates label defs on repo. """
+        existing_labels_map = {l.name: l for l in self._repo.get_labels()}
+
+        labels_to_update = [
+            l for l in labels
+            if l.name in existing_labels_map
+            and l != LabelParams.from_label(existing_labels_map[l.name])]
+        LOG.info(
+            f"Updating following labels on repo {self._user}/{self._name}: "
+            f"{labels_to_update}")
+        for l in labels_to_update:
+            existing_labels_map[l.name].edit(l.name, l.color, l.description)
+            LOG.debug(
+                f"Updated repo {self._user}/{self._name} label "
+                f"{existing_labels_map[l.name]} to: {l}")
+
+        labels_to_create = [
+            l for l in labels if l.name not in existing_labels_map]
+        for l in labels_to_create:
+            self._repo.create_label(l.name, l.color, l.description)
 
     def remove_labels(self, labels: list[str]):
-        existing_labels_map = {l.name: l for l in self.get_labels()}
+        """ Removes labels with the selected name. """
+        existing_labels_map = {l.name: l for l in self._repo.get_labels()}
+        existing_label_names = set(existing_labels_map.keys())
+
+        label_name_set = set(labels)
+        missing = label_name_set.difference(existing_label_names)
+        if missing:
+            msg = (
+                f"Requested deletion of repo {self._user}/{self._name} for "
+                f"non-existing labels: {missing}")
+            # TODO(aznashwan): optionally raise based on kwarg.
+            LOG.warn(msg)
+
+        to_delete = label_name_set.intersection(existing_label_names)
+        LOG.info(
+            f"Deleting following labels on repo {self._user}/{self._name}: "
+            f"{to_delete}")
+        for name in to_delete:
+            existing_labels_map[name].delete()
+            LOG.debug(f"Deleted repo {self._user}/{self._name} label '{name}'")
+
+
+
+class ObjectLabellingTarget(BaseLabelsTarget):
+    """ Abstracts managing pre-existing repo labels on repo objects like PRs and Issues. """
+
+    def __init__(self, client: Github, repo_user: str, repo_name: str,
+                 # NOTE(aznashwan): repo items IDs all start from 1:
+                 repo_item_type: str, repo_item_id):
+        if not all([repo_user, repo_name, repo_item_type, repo_item_id]):
+            raise ValueError(
+                "All arguments must be provided, got: "
+                f"{repo_name=} {repo_name=} {repo_item_type=} {repo_item_id=}")
+
+        self._client = client
+        self._user = repo_user
+        self._repo = repo_name
+        self._item_type = repo_item_type
+        self._item_id = repo_item_id
+        self._target_obj = self._get_target_obj()
+
+    def get_target_handle(self):
+        return self._target_obj
+
+    def _get_target_resource_path(self):
+        return f"{self._user}/{self._repo}/{self._item_type}/{self._item_id}"
+
+    def _get_target_obj(self) -> Issue|PullRequest:
+        repo = self._client.get_repo(f"{self._user}/{self._repo}")
+
+        target = repo
+        match self._item_type:
+            case _ if not self._item_type:
+                raise ValueError(f"{self._item_type=}")
+            case "issue" | "issues":
+                if self._item_id:
+                    target = repo.get_issue(self._item_id)
+                else:
+                    raise NotImplemented
+            case "pull" | "pulls":
+                if self._item_id:
+                    target = repo.get_pull(self._item_id)
+                else:
+                    raise NotImplemented
+            case _:
+                raise ValueError(
+                    f"Unsupported repo labelling target: {self._item_type}")
+
+        return target
+
+    def get_labels(self) -> list[LabelParams]:
+        return [LabelParams.from_label(l) for l in self._target_obj.get_labels()]
+
+    def set_labels(self, labels: list[LabelParams]):
+        # NOTE(aznashwan): `set_labels()` overwrites the whole label list,
+        # so we must union the label sets ourselves.
+        old_labels = self.get_labels()
+        label_names_to_add = {l.name for l in old_labels}.union(
+            {l.name for l in labels})
+
+        if label_names_to_add:
+            LOG.info(f"Adding following labels to "
+                     f"{self._get_target_resource_path()}: {label_names_to_add}")
+            self._target_obj.set_labels(*label_names_to_add)
+
+    def remove_labels(self, labels: list[str]):
+        existing_labels_map = {l.name: l for l in self._target_obj.get_labels()}
 
         for label in labels:
             label_obj = existing_labels_map.get(label)
